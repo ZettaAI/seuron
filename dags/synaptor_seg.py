85e983d1bda161df0120890467c08cf997547e69
NT <nturner@zetta.ai>
2022-03-03 14:31:05 -0500

    feat: [WIP] linted synaptor basic seg

--- dags/synaptor_seg.py
+++ dags/synaptor_seg.py
@@ -7,19 +7,26 @@ their completion through the RabbitMQ
   "scale up, wait until the workflow is finished, report results, and scale
   down".
 """
-import json
+import os
+from datetime import datetime
 
 from airflow import DAG
-from airflow.models import Variable
 from airflow.utils.weight_rule import WeightRule
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable, BaseOperator as Operator
 
 from worker_op import worker_op
 from igneous_and_cloudvolume import check_queue
-from helper_ops import mark_done_op, slack_message_op
+# from helper_ops import mark_done_op, slack_message_op
+from slack_message import task_retry_alert, task_failure_alert
 from helper_ops import scale_up_cluster_op, scale_down_cluster_op
-from param_default import synaptor_param_default, check_worker_image_labels
-from param_default import default_mount_path
+from param_default import synaptor_param_default  # , check_worker_image_labels
+from param_default import default_args, default_mount_path
 
+
+# hard-coding this for now
+MAX_CLUSTER_SIZE = 10
+default_synaptor_workspace = "/Synaptor/task_scripts"
 
 # Processing parameters
 # need to set a default so any airflow container can parse the dag (?)
@@ -29,87 +36,165 @@ param = Variable.get("synaptor_param", deserialize_json=True)
 
 
 # Python callables (for PythonOperators)
-def sanity_check(dag, param):
+def sanity_check_op() -> None:
     """Checks the uploaded processing parameters for problems."""
     pass
 
 
-# Op functions
-def worker_op(dag):
-    """Runs a worker until all tasks are complete."""
+def generate_ngl_link_op() -> None:
+    """Generates a neuroglancer link to view the results."""
     pass
 
 
-def drain_tasks_op():
-    """Drains any tasks sitting in the queue."""
+def check_image_labels_op(junk) -> None:
+    """A placeholder since the image labels aren't setup.
+
+    Will replace with param_default.check_worker_image_labels later.
+    """
+    pass
+
+
+# Op functions
+def synaptor_op(dag: DAG, param: dict, queue: str, i: int) -> Operator:
+    """Runs a synaptor worker until it receives a kill task."""
     from airflow import configuration as conf
     broker_url = conf.get("celery", "broker_url")
 
-    command = f'bash -c "drain_tasks {broker_url}"'
+    workspace_path = param.get("WORKSPACE_PATH", default_synaptor_workspace)
+    script_path = os.path.join(workspace_path, "worker.py")
 
+    command = f'/bin/sh -c "python -u {script_path} {broker_url} 900"'
+
+    # picking which variables to mount in the containers and where to put them
     variables = ["synaptor_param"]
     if "MOUNT_SECRETS" in param:
         variables += param["MOUNT_SECRETS"]
-
     mount_point = param.get("MOUNT_PATH", default_mount_path)
+
     return worker_op(
                variables=variables, mount_point=mount_point,
-               task_id='drain_tasks', command=command,
+               task_id=f"worker_{i}", command=command,
+               force_pull=True, on_retry_callback=task_retry_alert,
+               image=param["SYNAPTOR_IMAGE"], priority_weight=100_000,
+               weight_rule=WeightRule.ABSOLUTE, queue=queue, dag=dag)
+
+
+def drain_tasks_op() -> Operator:
+    """Drains any tasks sitting in the queue. Skipping this for now"""
+    pass
+
+
+def generate_tasks_op(dag: DAG,
+                      taskname: str,
+                      queue: str = "manager"
+                      ) -> Operator:
+    """Generates tasks to run and adds them to the RabbitMQ."""
+    from airflow import configuration as conf
+    broker_url = conf.get("celery", "broker_url")
+
+    workspace_path = param.get("WORKSPACE_PATH", default_synaptor_workspace)
+    script_path = os.path.join(workspace_path, "generator.py")
+
+    command = f'/bin/sh -c "python -u {script_path} {taskname} {broker_url}"'
+
+    # picking which variables to mount in the containers and where to put them
+    variables = ["synaptor_param"]
+    if "MOUNT_SECRETS" in param:
+        variables += param["MOUNT_SECRETS"]
+    mount_point = param.get("MOUNT_PATH", default_mount_path)
+
+    return worker_op(
+               variables=variables, mount_point=mount_point,
+               task_id=f"generate_{taskname}", command=command,
                force_pull=True, on_failure_callback=task_failure_alert,
                image=param["SYNAPTOR_IMAGE"], priority_weight=100_000,
                weight_rule=WeightRule.ABSOLUTE, queue=queue, dag=dag)
 
 
-def generate_tasks_op(dag, taskname):
-    """Generates tasks to run and adds them to the RabbitMQ."""
-    pass
+def wait_op(dag: DAG, taskname: str) -> Operator:
+    """Waits for a task to finish."""
+    return PythonOperator(
+               task_id=f"wait_for_queue_{taskname}",
+               python_callable=check_queue,
+               op_args=("synaptor",),
+               priority_weight=100_000,
+               weight_rule=WeightRule.ABSOLUTE,
+               queue="manager",
+               dag=dag)
 
 
-# Instantiating ops
+# DAG creation
+generator_default_args = {
+    "owner": "seuronbot",
+    "depends_on_past": False,
+    "start_date": datetime(2022, 2, 22),
+    "catchup": False,
+    "retries": 0,
+}
+
+dag_generator = DAG("synaptor_generator",
+                    default_args=generator_default_args,
+                    schedule_interval=None)
+dag_worker = DAG("synaptor_worker",
+                 default_args=default_args,
+                 schedule_interval=None)
+
+
+# Instantiating ops within DAGs
+## Meta-stuff (cluster management & checks)
 sanity_check = PythonOperator(
     task_id="sanity_check",
     python_callable=sanity_check_op,
-    op_args = tuple(),
+    priority_weight=100_000,
     on_failure_callback=task_failure_alert,
     weight_rule=WeightRule.ABSOLUTE,
     queue="manager",
     dag=dag_generator)
 
-scale_up_cluster = scale_up_cluster_op(
-        dag_worker, "synaptor", "cpu",
-        min(param.get("TASK_NUM", 1), 20),
-        min(param.get("TASK_NUM", 1), 20),  # not sure what these args mean yet
-        "cluster")
-scale_down_cluster = scale_down_cluster_op(
-        dag_worker, "synaptor", "cpu", 0, "cluster")
+check_image_labels = PythonOperator(
+    task_id="check_image_labels",
+    python_callable=check_image_labels_op,
+    op_args=("inference_param",),
+    on_failure_callback=task_failure_alert,
+    weight_rule=WeightRule.ABSOLUTE,
+    queue="manager",
+    dag=dag_generator)
 
-wait_for_queue = PythonOperator(
-    task_id="wait_for_queue",
-    python_callable=check_queue,
-    op_args=("synaptor",),
+generate_ngl_link = PythonOperator(
+    task_id="generate_ng_link",
+    python_callable=generate_ngl_link_op,
     priority_weight=100_000,
     weight_rule=WeightRule.ABSOLUTE,
     queue="manager",
-    dag=dag_worker
-)
+    dag=dag_generator)
 
-generate_chunk_ccs = generate_tasks_op(dag_worker, "chunk_ccs")
-generate_chunk_seg_map = generate_tasks_op(dag_worker, "merge_segs")
-generate_remap= generate_tasks_op(dag_worker, "remap")
+scale_up_cluster = scale_up_cluster_op(
+        dag_worker, "synaptor", "cpu", 1, MAX_CLUSTER_SIZE, "cluster")
+scale_down_cluster = scale_down_cluster_op(
+        dag_worker, "synaptor", "cpu", 0, "cluster")
 
-workers = [worker_op(dag, i) for i in range(param.get("TASK_NUM", 1))]
+## Actual work ops
+workers = [synaptor_op(dag_worker, i) for i in range(MAX_CLUSTER_SIZE)]
+
+generate_chunk_ccs = generate_tasks_op(dag_generator, "chunk_ccs")
+wait_chunk_ccs = wait_op(dag_generator, "chunk_ccs")
+
+generate_merge_segs = generate_tasks_op(dag_generator, "merge_segs")
+wait_merge_segs = wait_op(dag_generator, "merge_segs")
+
+generate_remap = generate_tasks_op(dag_generator, "remap")
+wait_remap = wait_op(dag_generator, "remap")
+
+generate_kill = generate_tasks_op(dag_generator, "kill")
 
 
-# DAG definition
+# Setting up dependencies
 ## Worker dag
 scale_up_cluster >> workers >> scale_down_cluster
 
-(scale_up_cluster >> wait_for_synaptor
- >> generate_ng_link >> scale_down_cluster)
-
-## Task dag
-(sanity_check >> image_parameters >> drain_tasks
- >> generate_chunk_ccs >> wait_for_queue
- >> generate_merge_segs >> wait_for_queue
- >> generate_remap >> wait_for_queue
- >> mark_done)
+## Generator/task dag
+(sanity_check >> check_image_labels  # >> drain_tasks  # skipping drain for now
+ >> generate_chunk_ccs >> wait_chunk_ccs
+ >> generate_merge_segs >> wait_merge_segs
+ >> generate_remap >> wait_remap
+ >> generate_kill >> generate_ngl_link)
